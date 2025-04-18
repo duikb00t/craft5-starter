08b2fcd0073bf81a291b93708369432e5cccb113
Kennes Ward <ward.kennes@ewings.be>
2025-04-18 15:30:53 +0200

    Updated deployer.php with npm install and build also added php carft clear-caches all and craft up

--- deploy.php
+++ deploy.php
@@ -15,29 +15,30 @@ set('git_tty', true);
 set('keep_releases', 4);
 set('shared_files', ['.env']);
 set('shared_dirs', ['storage']);
-//set('writable_dirs', ['storage', 'web/cpresources']);
 set('allow_anonymous_stats', false);
 
 // Hosts
 host('staging')
     ->setHostname('ssh101.webhosting.be')
     ->set('remote_user', 'imprentabe')
-    ->set('deploy_path', ' ~/subsites/staging.imprenta.be');
+    ->set('deploy_path', '~/subsites/staging.imprenta.be');
 
 // Tasks
-desc('Install dependencies');
+desc('Install PHP dependencies');
 task('deploy:vendors', function () {
     run('cd {{release_path}} && composer install --no-dev --quiet --prefer-dist --optimize-autoloader');
 });
 
-desc('Craft CMS: run project config sync');
-task('craft:project-config-sync', function () {
-    run('cd {{release_path}} && php craft project-config/sync --force');
+desc('Craft CMS: post-composer cleanup');
+task('craft:post-install', function () {
+    run('cd {{release_path}} && php craft clear-caches/all');
+    run('cd {{release_path}} && php craft up');
 });
 
-desc('Craft CMS: clear caches');
-task('craft:clear-caches', function () {
-    run('cd {{release_path}} && php craft cache/flush-all');
+desc('Install and build frontend assets');
+task('deploy:assets', function () {
+    run('cd {{release_path}} && npm install --legacy-peer-deps');
+    run('cd {{release_path}} && npm run build');
 });
 
 desc('Craft CMS: apply all pending migrations');
@@ -45,9 +46,21 @@ task('craft:migrate', function () {
     run('cd {{release_path}} && php craft migrate/all');
 });
 
+desc('Craft CMS: sync project config');
+task('craft:project-config-sync', function () {
+    run('cd {{release_path}} && php craft project-config/sync --force');
+});
+
+desc('Craft CMS: clear Craft caches again');
+task('craft:clear-caches', function () {
+    run('cd {{release_path}} && php craft cache/flush-all');
+});
+
 // Hooks
 after('deploy:shared', 'deploy:vendors');
-after('deploy:vendors', 'craft:migrate');
+after('deploy:vendors', 'craft:post-install');
+after('craft:post-install', 'deploy:assets');
+after('deploy:assets', 'craft:migrate');
 after('craft:migrate', 'craft:project-config-sync');
 after('craft:project-config-sync', 'craft:clear-caches');
 after('deploy:failed', 'deploy:unlock');
