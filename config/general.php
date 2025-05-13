242b33042d1a355c01851846619ba4d56c9269d2
Kennes Ward <ward.kennes@ewings.be>
2025-05-13 17:12:42 +0200

    Added CPresources to shared folder and resourceBasePath in general.php

--- config/general.php
+++ config/general.php
@@ -24,4 +24,6 @@ return GeneralConfig::create()
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
     ])
+    ->resourceBasePath(dirname(__DIR__) . '/web/cpresources')
+    ->resourceBaseUrl('/cpresources');
 ;
